5795179af263bf50ecc4c977e91c06a1b227c8e5
Rigelion <cupofbeer123@gmail.com>
2023-04-22 22:46:55 +0300

    feat: implement weather section

--- src/index.js
+++ src/index.js
@@ -3,7 +3,10 @@ window.onload = function () {
   showPrevSlideHandler();
   setBackgroundImage();
   showTime();
+  setWeatherSity();
+  getWeather();
 };
+localStorageHandler();
 
 /** Slider ***/
 
@@ -100,4 +103,47 @@ function localStorageHandler() {
   window.addEventListener("beforeunload", setLocalStorageName);
   window.addEventListener("load", getLocalStorage);
 }
-localStorageHandler();
+
+/** Weather */
+
+async function getWeather() {
+  const sity = document.querySelector(".weather__sity");
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${sity.value}&lang=en&appid=2c7a15d52fecce72f232d83719afee73&units=metric`;
+  const res = await fetch(url);
+  const data = await res.json();
+  getWeatherIcon(data);
+  getWeatherTemperature(data);
+  getWeatherDescription(data);
+  getWeatherWind(data);
+  getWeatherHumidity(data);
+}
+function getWeatherIcon(data) {
+  const weatherIcon = document.querySelector(".weather__icon");
+  weatherIcon.className = "weather__icon owf";
+  weatherIcon.classList.add(`owf-${data.weather[0].id}`);
+}
+function getWeatherTemperature(data) {
+  document.querySelector(".weather__temperature").textContent = `${Math.ceil(
+    data.main.temp
+  )} °C`;
+}
+function getWeatherDescription(data) {
+  document.querySelector(
+    ".weather__description"
+  ).textContent = `${data.weather[0].description}`;
+}
+function getWeatherWind(data) {
+  document.querySelector(
+    ".weather__wind"
+  ).textContent = `Wind speed: ${Math.ceil(data.wind.speed)} m/s`;
+}
+function getWeatherHumidity(data) {
+  document.querySelector(
+    ".weather__humidity"
+  ).textContent = `Humidity: ${Math.ceil(data.main.humidity)}%`;
+}
+function setWeatherSity() {
+  document
+    .querySelector(".weather__sity")
+    .addEventListener("change", getWeather);
+}
