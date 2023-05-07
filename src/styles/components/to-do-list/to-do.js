b794a5dd8f2c8fe81cf77e84ceca248c73200d56
Rigelion <cupofbeer123@gmail.com>
2023-05-07 19:36:19 +0300

    refactor: change structure of the todo-list

--- src/styles/components/to-do-list/to-do.js
+++ src/styles/components/to-do-list/to-do.js
@@ -8,7 +8,10 @@ const CssClasses = {
   OPEN: 'open',
   TODO_LIST: 'todo__current-list',
   DONE_LIST: 'todo__done-list',
-  EDIT_BUTTON: 'button__edit',
+  EDIT_MODE: 'edit-mode',
+  DONE: 'done',
+  DELETE_DONE: 'delete-done',
+  SAVE_DONE: 'save-done',
 }
 
 class ToDo {
@@ -27,7 +30,7 @@ class ToDo {
 
   todoContainerHandler() {
     this.todoIcon.addEventListener('click', () => {
-      this.todoIcon.classList.toggle('open')
+      this.todoIcon.classList.toggle(CssClasses.OPEN)
       this.todoContainer.classList.toggle(CssClasses.OPEN)
       this.popup.classList.toggle(CssClasses.OPEN)
       this.settingsButton.classList.remove(CssClasses.OPEN)
@@ -44,251 +47,140 @@ class ToDo {
       if (this.addInput.value) {
         this.renderNewTask(this.addInput.value)
         this.currentTasksStorage.push(this.addInput.value)
+        localStorage.setItem('currentTasks', JSON.stringify(this.currentTasksStorage));
         console.log(this.currentTasksStorage)
         this.addInput.value = ''
       }
     })
-
   }
 
   renderDoneTask(value) {
-    const currentTaskItem = document.createElement('li');
-    currentTaskItem.className = 'todo__current-item task-item';
-
-    const currentTaskDoneButton = document.createElement('button');
-    currentTaskDoneButton.className = 'todo__current-button button__done button done';
-    currentTaskDoneButton.textContent = 'Done';
-
-    const currentTaskLabel = document.createElement('div');
-    currentTaskLabel.className = 'todo__current-label';
-
-    const currentTaskInput = document.createElement('input');
-    currentTaskInput.className = 'todo__current-input task-input edit-mode';
-    currentTaskLabel.textContent = value;
-
-    const currentTaskEditButton = document.createElement('button');
-    currentTaskEditButton.className = 'todo__current-button button__edit button';
-    currentTaskEditButton.textContent = 'Edit';
-
-    const currentTaskSaveButton = document.createElement('button');
-    currentTaskSaveButton.className = 'todo__current-button button__save button edit-mode save-done';
-    currentTaskSaveButton.textContent = 'Save';
-
-    const currentTaskDeleteButton = document.createElement('button');
-    currentTaskDeleteButton.className = 'todo__current-button button__delete button delete-done';
-    currentTaskDeleteButton.textContent = 'Delete';
-    currentTaskItem.append(currentTaskDoneButton);
-    currentTaskItem.append(currentTaskLabel);
-    currentTaskItem.append(currentTaskInput);
-    currentTaskItem.append(currentTaskEditButton);
-    currentTaskItem.append(currentTaskSaveButton);
-    currentTaskItem.append(currentTaskDeleteButton);
-    this.currentDoneList.append(currentTaskItem);
-
-    currentTaskEditButton.addEventListener('click', () => {
-      currentTaskInput.value = currentTaskLabel.textContent
-      currentTaskLabel.classList.add('edit-mode');
-      currentTaskInput.classList.remove('edit-mode');
-      currentTaskEditButton.classList.add('edit-mode');
-      currentTaskSaveButton.classList.remove('edit-mode');
-    })
-
-    currentTaskSaveButton.addEventListener('click', () => {
-      // const index = this.currentTasksStorage.indexOf(currentTaskLabel.textContent);
-      // // const indexDone = this.doneTasksStorage.indexOf(currentTaskLabel.textContent);
-      // this.currentTasksStorage.splice(index, 1, `${currentTaskInput.value}`);
-
-
-      const index = this.currentTasksStorage.indexOf(currentTaskLabel.textContent);
-      const indexDone = this.doneTasksStorage.indexOf(currentTaskLabel.textContent);
-      if (!currentTaskSaveButton.classList.contains('save-done')) {
-        this.currentTasksStorage.splice(index, 1, `${currentTaskInput.value}`);
-      } else if (currentTaskSaveButton.classList.contains('save-done')) {
-        this.doneTasksStorage.splice(indexDone, 1, `${currentTaskInput.value}`);
-      }
-
-
-      currentTaskLabel.textContent = currentTaskInput.value
-      console.log(this.currentTasksStorage)
-      console.log(this.doneTasksStorage)
-      currentTaskLabel.classList.remove('edit-mode');
-      currentTaskInput.classList.add('edit-mode');
-      currentTaskEditButton.classList.remove('edit-mode');
-      currentTaskSaveButton.classList.add('edit-mode');
-    })
-    currentTaskDeleteButton.addEventListener('click', () => {
-      const parentOfDeleteButton = currentTaskDeleteButton.parentNode;
-      const parentOfNode = parentOfDeleteButton.parentNode;
-      const index = this.currentTasksStorage.indexOf(currentTaskLabel.textContent);
-      const indexDone = this.doneTasksStorage.indexOf(currentTaskLabel.textContent);
-      if (!currentTaskDeleteButton.classList.contains('delete-done')) {
-        this.currentTasksStorage.splice(index, 1);
-      }
-      if (currentTaskDeleteButton.classList.contains('delete-done')) {
-        this.doneTasksStorage.splice(indexDone, 1);
-      }
-      console.log(this.currentTasksStorage, this.doneTasksStorage)
-      parentOfNode.removeChild(parentOfDeleteButton);
-    })
-
-    currentTaskDoneButton.addEventListener('click', () => {
-      const parentOfDoneButton = currentTaskDoneButton.parentNode;
-      if (currentTaskDoneButton.classList.contains('done')) {
-        currentTaskSaveButton.classList.remove('save-done')
-        currentTaskDoneButton.classList.remove('done');
-        currentTaskDeleteButton.classList.remove('delete-done');
-        this.currentTodoList.appendChild(parentOfDoneButton)
-        const index = this.doneTasksStorage.indexOf(currentTaskLabel.textContent);
-        this.doneTasksStorage.splice(index, 1);
-        this.currentTasksStorage.push(currentTaskLabel.textContent);
-        console.log(this.doneTasksStorage)
-      } else {
-        this.currentDoneList.append(parentOfDoneButton);
-        currentTaskDoneButton.classList.add('done');
-        currentTaskDeleteButton.classList.add('delete-done');
-        currentTaskSaveButton.classList.add('save-done');
-        this.doneTasksStorage.push(currentTaskLabel.textContent);
-        const index = this.currentTasksStorage.indexOf(currentTaskLabel.textContent);
-        this.currentTasksStorage.splice(index, 1);
-        console.log(this.doneTasksStorage)
-      }
-    })
+    this.currentDoneList.innerHTML += `
+      <li class="todo__current-item task-item">
+        <button class="todo__current-button button__done button done">Done</button>
+        <div class="todo__current-label">${value}</div>
+        <input class="todo__current-input task-input edit-mode">
+        <button class="todo__current-button button__edit button">Edit</button>
+        <button class="todo__current-button button__save button edit-mode save-done">Save</button>
+        <button class="todo__current-button button__delete button delete-done">Delete</button>
+      </li>`
   }
 
   renderNewTask(value) {
-    const currentTaskItem = document.createElement('li');
-    currentTaskItem.className = 'todo__current-item task-item';
+    this.currentTodoList.innerHTML += `
+   <li class="todo__current-item task-item">
+      <button class="todo__current-button button__done button">Done</button>
+      <div class="todo__current-label">${value}</div>
+      <input class="todo__current-input task-input edit-mode">
+      <button class="todo__current-button button__edit button">Edit</button>
+      <button class="todo__current-button button__save button edit-mode">Save</button>
+      <button class="todo__current-button button__delete button">Delete</button>
+   </li>`;
+  }
 
-    const currentTaskDoneButton = document.createElement('button');
-    currentTaskDoneButton.className = 'todo__current-button button__done button';
-    currentTaskDoneButton.textContent = 'Done';
+  TaskButtonsHandler() {
+    this.todoContainer.addEventListener('click', (e) => {
+      const done = Array.from(e.target.parentNode.children)[0]
+      const label = Array.from(e.target.parentNode.children)[1]
+      const input = Array.from(e.target.parentNode.children)[2]
+      const edit = Array.from(e.target.parentNode.children)[3]
+      const save = Array.from(e.target.parentNode.children)[4]
+      const del = Array.from(e.target.parentNode.children)[5]
 
-    const currentTaskLabel = document.createElement('div');
-    currentTaskLabel.className = 'todo__current-label';
-
-    const currentTaskInput = document.createElement('input');
-    currentTaskInput.className = 'todo__current-input task-input edit-mode';
-    currentTaskLabel.textContent = value;
-
-    const currentTaskEditButton = document.createElement('button');
-    currentTaskEditButton.className = 'todo__current-button button__edit button';
-    currentTaskEditButton.textContent = 'Edit';
-
-    const currentTaskSaveButton = document.createElement('button');
-    currentTaskSaveButton.className = 'todo__current-button button__save button edit-mode';
-    currentTaskSaveButton.textContent = 'Save';
-
-    const currentTaskDeleteButton = document.createElement('button');
-    currentTaskDeleteButton.className = 'todo__current-button button__delete button';
-    currentTaskDeleteButton.textContent = 'Delete';
-
-    currentTaskItem.append(currentTaskDoneButton);
-    currentTaskItem.append(currentTaskLabel);
-    currentTaskItem.append(currentTaskInput);
-    currentTaskItem.append(currentTaskEditButton);
-    currentTaskItem.append(currentTaskSaveButton);
-    currentTaskItem.append(currentTaskDeleteButton);
-    this.currentTodoList.append(currentTaskItem);
-
-    currentTaskEditButton.addEventListener('click', () => {
-      currentTaskInput.value = currentTaskLabel.textContent
-      currentTaskLabel.classList.add('edit-mode');
-      currentTaskInput.classList.remove('edit-mode');
-      currentTaskEditButton.classList.add('edit-mode');
-      currentTaskSaveButton.classList.remove('edit-mode');
-    })
-
-    currentTaskSaveButton.addEventListener('click', () => {
-      // const index = this.currentTasksStorage.indexOf(currentTaskLabel.textContent);
-      // // const indexDone = this.doneTasksStorage.indexOf(currentTaskLabel.textContent);
-      // this.currentTasksStorage.splice(index, 1, `${currentTaskInput.value}`);
-
-
-      const index = this.currentTasksStorage.indexOf(currentTaskLabel.textContent);
-      const indexDone = this.doneTasksStorage.indexOf(currentTaskLabel.textContent);
-      if (!currentTaskSaveButton.classList.contains('save-done')) {
-        this.currentTasksStorage.splice(index, 1, `${currentTaskInput.value}`);
-      } else if (currentTaskSaveButton.classList.contains('save-done')) {
-        this.doneTasksStorage.splice(indexDone, 1, `${currentTaskInput.value}`);
+      if (e.target.closest('.button__edit')) {
+        input.value = label.textContent
+        label.classList.add(CssClasses.EDIT_MODE)
+        input.classList.remove(CssClasses.EDIT_MODE)
+        edit.classList.add(CssClasses.EDIT_MODE);
+        save.classList.remove(CssClasses.EDIT_MODE);
       }
 
-
-      currentTaskLabel.textContent = currentTaskInput.value
-      console.log(this.currentTasksStorage)
-      console.log(this.doneTasksStorage)
-      currentTaskLabel.classList.remove('edit-mode');
-      currentTaskInput.classList.add('edit-mode');
-      currentTaskEditButton.classList.remove('edit-mode');
-      currentTaskSaveButton.classList.add('edit-mode');
-    })
-
-    currentTaskDeleteButton.addEventListener('click', () => {
-      const parentOfDeleteButton = currentTaskDeleteButton.parentNode;
-      const parentOfNode = parentOfDeleteButton.parentNode;
-      const index = this.currentTasksStorage.indexOf(currentTaskLabel.textContent);
-      const indexDone = this.doneTasksStorage.indexOf(currentTaskLabel.textContent);
-      if (!currentTaskDeleteButton.classList.contains('delete-done')) {
-        this.currentTasksStorage.splice(index, 1);
+      if (e.target.closest('.button__done')) {
+        const index = this.currentTasksStorage.indexOf(label.textContent);
+        const indexDone = this.doneTasksStorage.indexOf(label.textContent);
+        const parentOfDoneButton = done.parentNode;
+        if (done.classList.contains(CssClasses.DONE)) {
+          this.currentTodoList.appendChild(parentOfDoneButton)
+          done.classList.remove(CssClasses.DONE);
+          del.classList.remove(CssClasses.DELETE_DONE);
+          save.classList.remove(CssClasses.SAVE_DONE)
+          this.doneTasksStorage.splice(indexDone, 1);
+          localStorage.setItem('doneTasks', JSON.stringify(this.doneTasksStorage));
+          this.currentTasksStorage.push(label.textContent);
+          localStorage.setItem('currentTasks', JSON.stringify(this.currentTasksStorage));
+          console.log(this.doneTasksStorage)
+        } else {
+          this.currentDoneList.append(parentOfDoneButton)
+          done.classList.add(CssClasses.DONE);
+          del.classList.add(CssClasses.DELETE_DONE);
+          save.classList.add(CssClasses.SAVE_DONE)
+          this.doneTasksStorage.push(label.textContent);
+          localStorage.setItem('doneTasks', JSON.stringify(this.doneTasksStorage));
+          this.currentTasksStorage.splice(index, 1);
+          localStorage.setItem('currentTasks', JSON.stringify(this.currentTasksStorage));
+          console.log(this.doneTasksStorage)
+        }
       }
-      if (currentTaskDeleteButton.classList.contains('delete-done')) {
-        this.doneTasksStorage.splice(indexDone, 1);
-      }
-      console.log(this.currentTasksStorage, this.doneTasksStorage)
-      parentOfNode.removeChild(parentOfDeleteButton);
-    })
 
-    currentTaskDoneButton.addEventListener('click', () => {
-      const parentOfDoneButton = currentTaskDoneButton.parentNode;
-      if (currentTaskDoneButton.classList.contains('done')) {
-        currentTaskDoneButton.classList.remove('done');
-        currentTaskDeleteButton.classList.remove('delete-done');
-        currentTaskSaveButton.classList.remove('save-done')
-        this.currentTodoList.appendChild(parentOfDoneButton)
-        const index = this.doneTasksStorage.indexOf(currentTaskLabel.textContent);
-        this.doneTasksStorage.splice(index, 1);
-        this.currentTasksStorage.push(currentTaskLabel.textContent);
+      if (e.target.closest('.button__save')) {
+        const index = this.currentTasksStorage.indexOf(label.textContent);
+        const indexDone = this.doneTasksStorage.indexOf(label.textContent);
+        if (!save.classList.contains(CssClasses.SAVE_DONE)) {
+          this.currentTasksStorage.splice(index, 1, `${input.value}`);
+          localStorage.setItem('currentTasks', JSON.stringify(this.currentTasksStorage));
+        } else if (save.classList.contains(CssClasses.SAVE_DONE)) {
+          this.doneTasksStorage.splice(indexDone, 1, `${input.value}`);
+          localStorage.setItem('doneTasks', JSON.stringify(this.doneTasksStorage));
+        }
+        label.textContent = input.value
+        console.log(this.currentTasksStorage)
         console.log(this.doneTasksStorage)
-      } else {
-        this.currentDoneList.append(parentOfDoneButton);
-        currentTaskDoneButton.classList.add('done');
-        currentTaskDeleteButton.classList.add('delete-done');
-        currentTaskSaveButton.classList.add('save-done');
-        this.doneTasksStorage.push(currentTaskLabel.textContent);
-        const index = this.currentTasksStorage.indexOf(currentTaskLabel.textContent);
-        this.currentTasksStorage.splice(index, 1);
-        console.log(this.doneTasksStorage)
+        label.classList.remove(CssClasses.EDIT_MODE);
+        input.classList.add(CssClasses.EDIT_MODE);
+        edit.classList.remove(CssClasses.EDIT_MODE);
+        save.classList.add(CssClasses.EDIT_MODE);
+      }
+
+      if (e.target.closest('.button__delete')) {
+        const parentOfDeleteButton = del.parentNode;
+        const parentOfNode = parentOfDeleteButton.parentNode;
+        const index = this.currentTasksStorage.indexOf(label.textContent);
+        const indexDone = this.doneTasksStorage.indexOf(label.textContent);
+        if (!del.classList.contains(CssClasses.DELETE_DONE)) {
+          this.currentTasksStorage.splice(index, 1);
+          localStorage.setItem('currentTasks', JSON.stringify(this.currentTasksStorage));
+        }
+        if (del.classList.contains(CssClasses.DELETE_DONE)) {
+          this.doneTasksStorage.splice(indexDone, 1);
+          localStorage.setItem('doneTasks', JSON.stringify(this.doneTasksStorage));
+        }
+        console.log(this.currentTasksStorage, this.doneTasksStorage)
+        parentOfNode.removeChild(parentOfDeleteButton);
       }
     })
   }
 
   localStorage() {
-
     if (localStorage.getItem('currentTasks') !== null) {
       this.currentTasksStorage = JSON.parse(localStorage.getItem('currentTasks'))
       this.currentTasksStorage.forEach(el => {
         this.renderNewTask(el);
       })
     }
-
-    if (localStorage.getItem('doneTasks') !== null ) {
+    if (localStorage.getItem('doneTasks') !== null) {
       this.doneTasksStorage = JSON.parse(localStorage.getItem('doneTasks'))
       this.doneTasksStorage.forEach(el => {
         this.renderDoneTask(el);
       })
     }
-    window.addEventListener("beforeunload", () => {
-      localStorage.setItem('currentTasks', JSON.stringify(this.currentTasksStorage));
-      localStorage.setItem('doneTasks', JSON.stringify(this.doneTasksStorage));
-    });
   }
-
-
 }
 
 const todo = new ToDo()
 todo.todoContainerHandler();
 todo.addNewTodoItem();
 todo.localStorage();
+todo.TaskButtonsHandler();
 
 
 
